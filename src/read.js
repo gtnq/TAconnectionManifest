13d570df93334bc666e9fccbf24e68c3f62c535b
Tony Yu <ttonyyu98@gmail.com>
2023-09-28 05:25:58 -0700

    going to combing everything into an array

--- src/read.js
+++ src/read.js
@@ -1,37 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import * as pdfjs from 'pdfjs-dist'
+import React, { useState, useEffect } from "react";
+import * as pdfjs from "pdfjs-dist";
 
 const PDFReader = ({ pdfUrl }) => {
-  const [text, setText] = useState('');
+	const [text, setText] = useState("");
+	const [all, setAll] = useState([]);
 
-  useEffect(() => {
-    const loadPdf = async () => {
-      pdfjs.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/build/pdf.worker.entry';
+	useEffect(() => {
+		const loadPdf = async () => {
+			pdfjs.GlobalWorkerOptions.workerSrc =
+				"//cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.js";
 
-      const loadingTask = pdfjs.getDocument(pdfUrl);
-      const pdf = await loadingTask.promise;
-      const pageNumber = 1;
-      const page = await pdf.getPage(pageNumber);
+			const loadingTask = pdfjs.getDocument(pdfUrl);
+			const pdf = await loadingTask.promise;
+            for (let i = 1, len = pdf.numPages; i < len; i += 2) {
+                const page = await pdf.getPage(i);
 
-      const content = await page.getTextContent();
+                const content = await page.getTextContent();
 
-      const strings = content.items.map(item => item.str);
-      setText(strings.join(' '));
-    };
+                const strings = content.items.map((item) => item.str);
+                setText(strings.join(" "));
+                setAll([...all, text]);
+            }
+            console.log(all)
+		};
 
-    loadPdf();
-    return () => {
-        URL.revokeObjectURL(pdfUrl);
-      };
-    }, [pdfUrl]);
+		loadPdf();
+		return () => {
+			URL.revokeObjectURL(pdfUrl);
+		};
+	}, [pdfUrl]);
 
-  return (
-    <div>
-      <div className="pdf-text">
-        {text}
-      </div>
-    </div>
-  );
-}
+	return (
+		<div>
+			<div className="pdf-text">{all}</div>
+		</div>
+	);
+};
 
 export default PDFReader;
